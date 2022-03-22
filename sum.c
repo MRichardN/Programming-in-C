bacb26605d75c9b645e41831f9196026602d6662
Mathenge Richard <40102996+MRichardN@users.noreply.github.com>
2022-03-22 14:42:37 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+// program to find sum of two numbers
+#include<stdio.h>
+int main ()
+{
+float x=10;
+float y=20;
+float sum;
+sum = x + y ;
+printf("The sum of 2 numbers is %f",sum);
+
+
+
+  return 0;
+}
